3ad34f6d937e09f90a7ee3218880ac75c53d1b63
Isaac  Middleton <ibo.middleton@gmail.com>
2020-03-03 20:36:19 -0500

    mvp

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -8,11 +8,13 @@ def selection_sort( arr ):
                 cur_index = j
         if cur_index != i:
             arr[i], arr[cur_index] = arr[cur_index], arr[i]
-        smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
         # TO-DO: swap
     return arr
+
+myList = [3,1,5,2,4,7,6]
+# print(selection_sort(myList))
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
@@ -22,7 +24,7 @@ def bubble_sort( arr ):
                 arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
 
-
+# print(bubble_sort(myList))
 
 # STRETCH: implement the Count Sort function below
 def count_sort( arr, maximum=-1 ):
--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,18 +1,38 @@
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
 def merge( arrA, arrB ):
-    elements = len( arrA ) + len( arrB )
-    merged_arr = [0] * elements
+    i = 0
+    j = 0
+    lenA = len(arrA)
+    lenB = len(arrB)
+    arr = []
     # TO-DO
+    while ((i<lenA) and (j<lenB)):
+        if arrA[i] < arrB[j]:
+            arr.append(arrA[i])
+            i = i + 1
+        else:
+            arr.append(arrB[j])
+            j = j + 1
     
-    return merged_arr
+    while(i < lenA):
+        arr.append(arrA[i])
+        i = i + 1
 
+    while(j < lenB):
+        arr.append(arrB[j])
+        j = j + 1
+    return arr
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
     # TO-DO
+    if len(arr)>1:
+        mid = len(arr)//2
+        left = arr[:mid]
+        right = arr[mid:] 
 
+        return merge(merge_sort(left), merge_sort(right))
     return arr
-
 
 # STRETCH: implement an in-place merge sort algorithm
 def merge_in_place(arr, start, mid, end):
